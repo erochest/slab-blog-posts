939ab77fedc930ca1ef0714f413ecfedaaaa917b
Eric Rochester <err8n@virginia.edu>
2012-08-20 16:30:04 -0400

    Fix tool path problem.

--- bin/mdpost2html.sh
+++ bin/mdpost2html.sh
@@ -3,6 +3,6 @@
 SRC="$1"
 DEST=${SRC%.md}.html
 
-pandoc --smart -f markdown -t html5 $SRC | ./fix_code.py > $DEST
+pandoc --smart -f markdown -t html5 $SRC | ./bin/fix_code.py > $DEST
 cat $DEST | pbcopy
 open $DEST
