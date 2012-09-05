87e556ea18627551b0fa5f3d8cd10ce7ef3f2781
Eric Rochester <err8n@virginia.edu>
2012-09-05 14:47:16 -0400

    Fix sourcecode munging.

--- bin/fix_code.py
+++ bin/fix_code.py
@@ -6,14 +6,14 @@ import sys
 
 def fix_pre(match):
     buffer = ['\n[sourcecode']
-    if match.group(1) and match.group(2):
-        buffer.append('{0}"{1}"'.format(match.group(1), match.group(2)))
-    buffer.append(']')
+    if match.group('lang'):
+        buffer.append(' language="{0}"'.format(match.group('lang')))
+    buffer.append(']\n')
     return ''.join(buffer)
 
 
 for line in sys.stdin:
-    line = re.sub(r'<pre><code>\[sourcecode(\s+\w+=)?(\w+)?\]', fix_pre, line)
+    line = re.sub(r'<pre(?: class="(?P<lang>\w+)")><code>', fix_pre, line)
     line = re.sub(r'-m &quot;(.*)&quot;', r'-m "\1"', line)
-    line = re.sub(r'\[/sourcecode\]</code></pre>', '[/sourcecode]\n', line)
+    line = re.sub(r'</code></pre>', '\n[/sourcecode]\n', line)
     sys.stdout.write(line)
